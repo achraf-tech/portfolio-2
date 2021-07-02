e23827aedbf8d2b09c44b6cc5eaeb737cdd023ad
Achref ben kadaa <achref>
2021-07-02 11:53:45 +0200

    achref

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const featuredPortfolio = [
+  {
+    id: 1,
+    title: "portfolio",
+    img: "https://www.slidesalad.com/wp-content/uploads/2019/04/Free-Business-Portfolio-Google-Slides-Templates-1.jpg",
+  },
+];
+
+export const webPortfolio = [
+  {
+    id: 6,
+    title: "amazon app react",
+    img: "https://d1fmx1rbmqrxrr.cloudfront.net/cnet/i/edit/2018/07/prime-video-770.jpg",
+  },
+];
+
+export const mobilePortfolio = [
+  {
+    id: 6,
+    title: "Mobile Banking App",
+    img: "https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900",
+  },
+];
